0f9c244b73b80f56d888e42ee382c427559d7391
Renan da Mota Ciciliato <renanciciliato@gmail.com>
2022-08-10 18:55:05 -0300

    Ajuste arredondamento média

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.post('/media', (req, res) => {
   if (isNaN(n1) || isNaN(n2)) {
     return res.status(400).json({ error: 'Invalid numbers' });
   }      
-  const media = Math.round((n1 + n2) / 2);  
+  const media = Math.round(((n1 + n2) / 2) * 100) / 100;  
   return res.json({
     n1,
     n2,
